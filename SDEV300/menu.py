5463eca80ca7d8e2c430afe095cc51873ed93aaa
jd1224 <Joshua.coe11@yahoo.com>
2020-08-27 08:06:27 -0400

    Update menu.py
    Psh

--- SDEV300/menu.py
+++ SDEV300/menu.py
@@ -6,13 +6,15 @@ It will then print the formatted output
 to the stdout
 Created by Joshua Coe
 SDEV300
-
 """
 
-import random
 import platform
 from os import system
 import sys
+import time
+import string
+import secrets
+import datetime
 
 def print_head():
     '''
@@ -33,7 +35,7 @@ def clear_screen():
     clears the screen after checking which
     system architecture is used.
     used to clean up input and stop it from
-    leaving a long string of incorrect answers
+    leaving a long string text
 
     '''
 
@@ -71,19 +73,154 @@ def check_quit(variable):
             'Thank you for using my app. Goodbye!')
 
 def password():
+    '''
+    Function to user for variables to
+    create a password
+    '''
     print_head()
-    print("passing the word")
+    options = {'passLength': 0, 'Uppercase Letters': 'n',\
+'Lowercase Letters': 'n', 'Numbers': 'n', 'Special Characters': 'n'}
+    try:
+        pass_length_get = input("How many characters long do you want your password to be? ")
+        check_quit(pass_length_get)
+        pass_length_get = int(pass_length_get)
+        options.update({'passLength': pass_length_get})
+    except ValueError:
+        check_return("You must choose an integer for length!")
 
-    check_return("results")
+    print("Choose the characters you would like to include.")
+    for setting in options:
+        if setting != 'passLength':
+            choosing = True
+            choice_count = 0
+            while choosing:
+                answer = input(setting+"(y/n)? ").lower()
+                check_quit(answer)
+                if answer in ['y', 'n']:
+                    choosing = False
+                else:
+                    if choice_count < 2:
+                        print("Enter only Y or N!")
+                        choice_count += 1
+                    else:
+                        check_return("Too many bad selections.")
+
+            options.update({setting: answer})
+
+    password_out = make_password(options)
+    check_return("Your Password is: "+password_out)
+
+def make_password(settings):
+    '''
+    Function to construct a password
+    Parameters:
+        settings dict containing the choices needed
+    Returns:
+        password string in the length and specs provided
+    '''
+    length = settings['passLength']
+    constructed_password = ''
+    character_set = ''
+    if settings['Uppercase Letters'].lower() == 'y':
+        character_set += string.ascii_uppercase
+    if settings['Lowercase Letters'].lower() == 'y':
+        character_set += string.ascii_lowercase
+    if settings['Numbers'].lower() == 'y':
+        character_set += string.digits
+    if settings['Special Characters'].lower() == 'y':
+        character_set += string.punctuation
+    print(len(character_set))
+    if len(character_set) != 0:
+        constructed_password = ''.join(secrets.choice(character_set) for i in range(length))
+    else:
+        check_return("You did not enter any characters to choose from!")
+
+    return constructed_password
+
+def percentage():
+    '''
+    Function to calculate and format
+    a percentage given a numerator and
+    denominator
+    '''
+    numerator = 0
+    denominator = 0
+    places = 0
+    choice_count = 0
+    print("Please use only integers with this function")
+    while True:
+        try:
+            numerator = input("Enter the numerator: ")
+            check_quit(numerator)
+            numerator = int(numerator)
+        except ValueError as e:
+            if choice_count < 2:
+                print("Only integers (Whole Numbers)!")
+                choice_count += 1
+            else:
+                check_return("Too many bad selections.")
+        try:
+            denominator = input("Enter the denominator: ")
+            check_quit(denominator)
+            denominator = int(denominator)
+        except ValueError as e:
+            if choice_count < 2:
+                print("Only integers (Whole Numbers)!")
+                choice_count += 1
+            else:
+                check_return("Too many bad selections.")
+        try:
+            places = input("How many decimal places: ")
+            check_quit(places)
+            places = int(places)
+            break
+        except ValueError as e:
+            if choice_count < 2:
+                print("Only integers (Whole Numbers)!")
+                choice_count += 1
+            else:
+                check_return("Too many bad selections.")
+    calculated_percentage = (numerator/denominator)
+    formatted = "{:.{places_after}%}".format(calculated_percentage, places_after=places)
+    check_return(str(numerator)+"/"+str(denominator)+" = "+str(formatted))
+
+
+def daysto4july():
+    '''
+    fucntion to calculate the number of days
+    from today until july 4th 2025
+    '''
+    date_future = datetime.date(2025, 7, 4)
+    date_today = datetime.date.today()
+    days_between = date_future - date_today
+    check_return("There are "+str(days_between.days)+" days between today and 4 July 2025.")
+
+def triangle():
+    '''
+    Function to calculate the leg of
+    triangle using the law of cosines
+    '''
+    pass
+
+def cylinder():
+    '''
+    Function to calculate the volume
+    of a right cylinder
+    '''
+    pass
 
 def check_return(message):
+    '''
+    Function to display the output of the previous function
+    and ask the user if they want to return to the menu or
+    quit the program.
+    '''
     print_head()
     print('\n')
     print(message)
     print('\n')
     print('---------------------------------------------')
     print('\n')
-   
 
     print("1. Return to menu")
     print("2. Exit the program")
@@ -112,7 +249,7 @@ def menu_screen():
 
         print('1. Generate Password')
         print('2. Calulate Percentage')
-        print('3. Days til 4 July 2025')
+        print('3. Days until 4 July 2025')
         print('4. Leg of a Triangle')
         print('5. Volume of a Cylinder')
         print('\n')
@@ -125,13 +262,17 @@ def menu_screen():
             password()
             choosing = False
         elif choice == '2':
-            print("percentage")
+            percentage()
+            choosing = False
         elif choice == '3':
-            print("days to 4j")
+            daysto4july()
+            choosing = False
         elif choice == '4':
-            print("Triangle")
+            triangle()
+            choosing = False
         elif choice == '5':
-            print("Cylinder")
+            cylinder()
+            choosing = False
         else:
             print("XX-->You made an invalid choice<--XX")
             input("XX-->  Press Enter to continue <--XX")
